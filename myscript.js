890e2ba197df1f4b6ef35c5b3e090d7c37281717
Uku <Ukusaarm@gmail.com>
2016-04-03 02:39:46 +0300

    showing rss on the site

--- myscript.js
+++ myscript.js
@@ -1,15 +1,21 @@
-YUI().use('yql', function(Y){
-    var query = 'select * from rss where url = "https://www.readability.com/rseero/latest/feed"'
-    var q = Y.YQL(query, function(r){
-        console.log(r);
-        var items = [];
-        $.each( r, function( key, val ) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
+        YUI().use('yql', function(Y){
+            var query = 'select * from rss where url = "https://www.readability.com/rseero/latest/feed"'
+            var q = Y.YQL(query, function(r){
+                console.log(r);
+                var results = r.query.results.item;
+                for (var i = 0; i < results.length; i++) {
+                  var feedItem = results[i];
+                  $( ".RSS-Feed").append('<div class="col-md-4">'+
+                '<div class="RSS-Element">'+ '<h1>' + feedItem.title + '</h1>' + feedItem.description + '</div>'+
+                "</div>");
+                }
         });
+            })
 
-        $( "<ul/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
-});
-    })
+
+
+            // $( "<div/>", {
+            //   "class": "col-md-4 RSS-Element",
+            //   html: feedItem.title +
+            //   feedItem.description
+            // }).appendTo( ".RSS-Feed" );
